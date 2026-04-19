4430fd0dd99d0cf435a8fb6c2ae0676b3607890a
mulirous <muriloalimacosta@hotmail.com>
2026-04-18 21:36:04 -0300

    refactor(main): Main clean

--- refactored/src/Main.java
+++ refactored/src/Main.java
@@ -1,7 +1,4 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    // inicia programa
     public static void main(String[] args) {
         Sistema.menu();
     }
